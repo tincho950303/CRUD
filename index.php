5e61ba706d54485c1010873cf0d1a8f9d2a7b7fd
Carlos Martin <105653194+tincho950303@users.noreply.github.com>
2025-05-22 22:12:00 -0300

    Iniciar proyecto, conexion, estilos y la base

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'database.php'?>
+<?php include 'includes\header.php'?>
+    <main>
+        <form action="guardar_tarea.php" method="POST">
+            <div class="form-group">
+                <input type="text" name="titulo" placeholder="Tarea" autofocus>                
+            </div>
+            <div class="form-group">
+                <textarea name="descripcion" id="" placeholder="Agrega una descripción"></textarea>               
+            </div>
+            <input type="submit" value="Guardar" name="guardar_tarea">
+        </form>
+    </main>
+
+<?php include 'includes\footer.php'?>
