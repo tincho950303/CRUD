40ffc5962301f5a52ac39ed656d4081f25ff1827
Carlos Martin <105653194+tincho950303@users.noreply.github.com>
2025-05-31 14:09:56 -0300

    Mostrar la tabla de las tareas

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php include 'database.php'?>
 <?php include 'includes\header.php'?>
     <main>
+        
+       
         <form action="guardar_tarea.php" method="POST">
             <div class="form-group">
                 <input type="text" name="titulo" placeholder="Tarea" autofocus>                
@@ -13,6 +15,29 @@
             </div>
             <input type="submit" value="Guardar" name="guardar_tarea">
         </form>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Titulo</th>
+                    <th>Descripción</th>
+                    <th>Fecha</th>                    
+                </tr>
+            </thead>
+            <tbody>
+                <?php $query = "SELECT * FROM tareas";
+                $resultado_tareas = mysqli_query($conexion,$query);
+                
+                while ($row = mysqli_fetch_array($resultado_tareas)) { ?>
+                    <tr>
+                        <td><?php echo $row['titulo'] ?></td>
+                        <td><?php echo $row['descripcion'] ?></td>
+                        <td><?php echo $row['fecha'] ?></td>
+                    </tr>                                 
+
+             <?php }?>
+            </tbody>
+        </table>
     </main>
 
 <?php include 'includes\footer.php'?>
